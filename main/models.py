5e7f700c83acddd8203386cd46edf4455dcc7569
Jose Maria Micoli <josemaria.micoli@gmail.com>
2023-08-12 18:43:54 -0300

    Added the models classes and add the models to admin file

--- main/models.py
+++ main/models.py
@@ -1,3 +1,74 @@
 from django.db import models
+from django.template.defaultfilters import Slugify
+from django_resized import ResizedImageField
+from django.utils import timezone
+from uuid import uuid4
+from django.conf import settings
+from django.urls import reverse
+import json
+import os
 
 # Create your models here.
+class Category(models.Model):
+    #main Category variable
+    title = models.CharField(null=True, blank=True, max_length=300)
+    
+    #Utility variables
+    uniqueId = models.CharField(null=True, blank=True, max_length=100)
+    slug = models.SlugField(null=True, blank=True, unique=True, max_length=500)
+    date_created = models.DateTimeField(null=True, blank=True)
+    last_updated = models.DateTimeField(null=True, blank=True)
+    
+    def __str__(self):
+        return '{} {}'.format(self.title, self.uniqueId)
+        
+    def get_absolute_url(self):
+        return reverse('category-detail', kwargs={'slug': self.slug})
+        
+    def save(self, *args, **kwargs):
+        if self.date_created is None:
+            self.date_created = timezone.localtime(timezone.now())
+        if self.uniqueId is None:
+            self.uniqueId = str(uuid4()).split('-')[4]
+            self.slug = slugify('{} {}'.format(self.title, self.uniqueId))
+            
+            self.slug = slugify('{} {}'.format(self.title, self.uniqueId))
+            self.last_updated = timezone.localtime(timezone.now())
+            super(Category, self).save(*args, **kwargs)
+            
+class Image(models.Model):
+    description = models.TextField(null=True, blank=True)
+    altText = models.TextField(null=True, blank=True)
+    hashtag = models.CharField(null=True, blank=True, max_length=300)
+    
+    #Image Fields
+    squareImage = ResizedImageField(size=[1000, 1000], crop=['middle', 'center'], default='default_square.jpg', upload_to='square')
+    landscapeImage = ResizeImageField(size=[2678, 1618], crop=['middle', 'center'], default='default_land.jpg', upload_to='landscape')
+    tallImage = ResizedImageField(size=[1618, 2878], crop=['middle', 'center'], default='default_tall.jpg', upload_to='tall')
+    
+    
+    #Related Fields
+    category = models.ForeignKey(Category, null=true, blank=True, on_delete=models.CASCADE)
+    
+    #Utility variables
+    uniqueId = models.CharField(null=True, blank=True, max_length=100)
+    slug = models.SlugField(null=True, blank=True, unique=True, max_length=500)
+    date_created = models.DateTimeField(null=True, blank=True)
+    last_updated = models.DateTimeField(null=True, blank=True)
+    
+    def __str__(self):
+        return '{} {}'.format(self.category, self.title, self.uniqueId)
+        
+    def get_absolute_url(self):
+        return reverse('image-detail', kwargs={'slug', self.slug})
+        
+    def save(self, *args, **kwargs):
+        if self.date_created is None:
+            self.date_created = timezone.localtime(timezone.now())
+        if self.uniqueId is None:
+            self.uniqueId = str(uuid4()).split('-')[4]
+            self.slug = slugify('{} {}'.format(self.title, self.uniqueId))
+            
+            self.slug = slugify('{} {}'.format(self.title, self.uniqueId))
+            self.last_updated = timezone.localtime(timezone.now())
+            super(Category, self).save(*args, **kwargs)
