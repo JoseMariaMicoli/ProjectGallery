002adc710b4d1e9505eed4724f3859d6cff0901b
Jose Maria Micoli <josemaria.micoli@gmail.com>
2023-08-15 20:35:17 -0300

    Added a fixed navbar in all html files with a link to email contact form page

--- main/forms.py
+++ main/forms.py
@@ -6,5 +6,5 @@ class ContactForm(forms.Form):
     name = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'placeholder': 'Name', 'style': 'width: 300px;', 'class': 'form-control'}))
     email = forms.CharField(validators=[EmailValidator()], widget=forms.EmailInput(attrs={'placeholder' :'Email', 'style': 'width: 300px;', 'class': 'form-control'}))
     phone = forms.CharField(max_length=15, widget=forms.TextInput(attrs={'placeholder': 'Phone', 'style': 'width: 300px;', 'class': 'form-control'}))
-    subject = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'placeholder': 'Phone', 'style': 'width: 300px;', 'class': 'form-control'}))
-    message = forms.CharField(widget=forms.Textarea(attrs={'placeholder':'Message', 'style': 'width: 300px;', 'class': 'form-control'}), required=True)
+    subject = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'placeholder': 'Subject', 'style': 'width: 300px;', 'class': 'form-control'}))
+    message = forms.CharField(widget=forms.Textarea(attrs={'placeholder': 'Message', 'style': 'width: 300px;', 'class': 'form-control'}), required=True)
