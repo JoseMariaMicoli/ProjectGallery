4ac0689470e7446f96743039b02874937baf4b1e
Jose Maria Micoli <josemaria.micoli@gmail.com>
2023-08-17 21:32:53 -0300

    Added subscription form page with Bootstrap stylesheet

--- subscription/forms.py
+++ subscription/forms.py
@@ -1,11 +1,14 @@
 from django import forms
+from django.core.validators import EmailValidator
+from django.forms import TextInput, EmailInput
+from main.models import Gender
 
 class SubscriptionForm(forms.Form):
-    first_name
-    last_name
-    birth_date
-    age
-    phone
-    email
-    dni_number
-    gender
+    first_name = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'placeholder': 'First Name', 'style': 'width: 300px;', 'class': 'form-control'}), required=True)
+    last_name = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'placeholder': 'Last Name', 'style': 'width: 300px;', 'class': 'form-control'}), required=True)
+    birth_date = forms.DateField(label='Birth Date', widget=forms.SelectDateWidget(attrs={'placeholder': 'Birth Date', 'style': 'width: 300px;', 'class': 'form-control'}), required=True)
+    age = forms.IntegerField(widget=forms.TextInput(attrs={'placeholder': 'Age', 'style': 'width: 300px;', 'class': 'form-control'}), required=True)
+    phone = forms.CharField(max_length=15, widget=forms.TextInput(attrs={'placeholder': 'Phone', 'style': 'width: 300px;', 'class': 'form-control'}), required=True)
+    email = forms.CharField(validators=[EmailValidator()], widget=forms.EmailInput(attrs={'placeholder' :'Email', 'style': 'width: 300px;', 'class': 'form-control'}), required=True)
+    dni_number = forms.CharField(max_length=8, widget=forms.TextInput(attrs={'placeholder': 'D.N.I.', 'style': 'width: 300px;', 'class': 'form-control'}), required=True)
+    gender = forms.ModelChoiceField(queryset=Gender.objects.all(), widget=forms.Select(attrs={'placeholder': 'Gender', 'style': 'width: 300px;', 'class': 'bootstrap-select'}), required=True)
--- subscription/views.py
+++ subscription/views.py
@@ -1,5 +1,21 @@
 from django.shortcuts import render
+from .models import *
+from .forms import SubscriptionForm
 
 # Create your views here.
 def subscriptionView(request):
-    return render(request, 'subscriptions/subscriptions.html', {})
+    if request.method == 'GET':
+        form = SubscriptionForm()
+    else:
+        form = SubscriptionForm(request.POST)
+        if form.is_valid():
+            first_name = form.cleaned_data["first_name"]
+            last_name = form.cleaned_data["last_name"]
+            birth_date = form.cleaned_data["birth_date"]
+            age = form.cleaned_data["age"]
+            phone = form.cleaned_data["phone"]
+            email = form.cleaned_data["email"]
+            dni_number = form.cleaned_data["dni_number"]
+            gender = form.cleaned_data["gender"]
+    
+    return render(request, "subscriptions/subscriptions.html", {"form": form})
