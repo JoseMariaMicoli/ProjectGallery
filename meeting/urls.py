92b8868c86f10ab2fd8f941cb6e1039f081a1979
Jose Maria Micoli <josemaria.micoli@gmail.com>
2023-09-10 02:47:02 +0000

    Solve the problem with the last commit

--- meeting/urls.py
+++ meeting/urls.py
@@ -2,7 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('register/', views.registerView, name='register'),
-    path('login/', views.loginView, name='login'),
-    path('dashboard', views.dashboardView, name='dashboard'),
+    path('meeting/register/', views.registerView, name='register'),
+    path('meeting/login/', views.loginView, name='login'),
+    path('meeting/dashboard/', views.dashboardView, name='dashboard'),
+    path('meeting/meeting/', views.videocall, name='meeting'),
+    path('meeting/logout/', views.logoutView, name='logout'),
+    path('meeting/join/', views.join_room, name='join_room'),
+    path('meeting/', views.index, name='index'),
 ]
--- meeting/views.py
+++ meeting/views.py
@@ -4,6 +4,9 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+def index(request):
+    return render(request, 'meeting/index.html')
+
 def registerView(request):
     if request.method == 'POST':
         form = RegisterForm(request.POST)
@@ -12,7 +15,7 @@ def registerView(request):
             return render(request, 'meeting/login.html', {'success': "Registration successful. Please login."})
         else:
             error_message = form.errors.as_text()
-            return render(request, 'register.html', {'Error': error_message})
+            return render(request, 'meeting/register.html', {'Error': error_message})
         
     return render(request, 'meeting/register.html')
 
@@ -23,7 +26,7 @@ def loginView(request):
         user = authenticate(request, username=email, password=password)
         if user is not None:
             login(request, user)
-            return redirect("/dashboard")
+            return redirect("/meeting/dashboard/")
         else:
             return render(request, 'meeting/login.html', {'error': "Invalid credentials. Please try again."})
 
@@ -31,4 +34,20 @@ def loginView(request):
 
 @login_required
 def dashboardView(request):
-    return render(request, 'meeting/dashboard.html')
+    return render(request, 'meeting/dashboard.html', {'name': request.user.first_name + " " + request.user.last_name})
+
+@login_required
+def videocall(request):
+    return render(request, 'meeting/videocall.html', {'name': request.user.first_name + " " + request.user.last_name})
+
+@login_required
+def logoutView(request):
+    logout(request)
+    return redirect("/meeting/login/")
+
+@login_required
+def join_room(request):
+    if request.method == 'POST':
+        roomID = request.POST['roomID']
+        return redirect("/meeting/meeting/?roomID=" + roomID)
+    return render(request, 'meeting/joinroom.html')
